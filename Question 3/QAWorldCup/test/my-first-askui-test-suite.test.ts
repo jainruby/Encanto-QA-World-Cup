22fa536729663cd7eab75db3066ef7efd0a342f6
abhishek-nagarro <31957475+abhishek-nagarro@users.noreply.github.com>
2023-04-17 18:08:35 +0530

    update

--- Question 3/QAWorldCup/test/my-first-askui-test-suite.test.ts
+++ Question 3/QAWorldCup/test/my-first-askui-test-suite.test.ts
@@ -1,10 +1,34 @@
 import { aui } from './helper/jest.setup';
 
-describe('jest with askui', () => {
+describe('QA World Cup with AskUI', () => {
 
-  it('should ask ginger a question', async () => {
+  it('Open Microsoft Teams', async () => {
+
+    await aui.pressTwoKeys('alt', 'space').exec();
+    await aui.type("Microsoft Teams").exec();
+    await aui.pressKey("enter").exec();
+
+  });
+
+  it('Open Ginger in Microsoft teams', async () => {
+
+    await aui.moveMouseTo().text().withText("Chat").below().text().withText("Activity").exec();
+    await aui.mouseLeftClick().exec();
+
+    await aui.moveMouseTo().text().withText("Ginger").exec();
+    await aui.mouseLeftClick().exec();
  
   });
+
+  it('Search NQLB in Ginger', async () => {
+
+    await aui.typeIn("What is NQLB?").textfield().contains().text().withText("Type a new message").exec();
+    await aui.mouseLeftClick().exec();
+
+    await aui.annotate();
+ 
+  });
+
 
 
 });
